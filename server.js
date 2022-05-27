e2437bc7594fb6d1db1da7a27b8c04b038e91fc3
Rahim Shaikh <twicelyrics958@gmail.com>
2022-05-27 18:11:20 +0530

    made home page

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-const routes = require('./src/routes/web');
+const hbs = require('hbs');
 
-app.use('view engine','hbs');
-// app.use('views',path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'hbs')
 
-app.use('',routes)
+app.use(require('./src/routes/web'))
+
+hbs.registerPartials(path.join(__dirname,'views/partials'))
+
+app.use(express.static('assets'))
 
 app.listen( PORT , ()=>{
     console.log(`Sever listegning on post http://localhost:${PORT}`)
